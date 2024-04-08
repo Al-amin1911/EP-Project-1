244cd92aa94ba922f436ef5d989feddde2f4e186
Al-amin <111284227+Al-amin1911@users.noreply.github.com>
2024-04-08 17:38:03 +0100

    Delete CODE directory

--- CODE/flask web app/website/home.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template, abort
-from flask_login import login_required, current_user
-
-from .__init__ import db
-#Define the blueprint: 'home', set
-home = Blueprint('home', __name__)
-
-
